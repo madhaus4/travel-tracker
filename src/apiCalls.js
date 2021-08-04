a8f29e4dec438bf2d93eade00cf8f86fb5533692
Maria DelSignore <maria.ad27@gmail.com>
2021-08-04 17:47:17 -0600

    Create fetch functions

---
+++ src/apiCalls.js
@@ -0,0 +1,39 @@
+const retrieveTravelersData = () => {
+  return fetch('http://localhost:3001/api/v1/travelers')
+    .then(response => response.json())
+    .then(data => data)
+    .catch(err => console.log('Error upon us'));
+}
+
+const retrieveCurrentUserData = () => {
+  return fetch(`http://localhost:3001/api/v1/travelers/${USER-ID-HERE}`)
+    .then(response => response.json())
+    .then(data => data)
+    .catch(err => console.log('Error upon us'));
+}
+
+const retrieveTripsData = () => {
+  return fetch('http://localhost:3001/api/v1/trips')
+  .then(response => response.json())
+  .then(data => data)
+  .catch(err => console.log('Error upon us'));
+}
+
+const retrieveDestinationsData = () => {
+  return fetch('http://localhost:3001/api/v1/destinations')
+  .then(response => response.json())
+  .then(data => data)
+  .catch(err => console.log('Error upon us'));
+}
+
+const getData = () => {
+  return Promise.all([
+    retrieveTravelersData(), 
+    retrieveCurrentUserData(), 
+    retrieveTripsData(), 
+    retrieveDestinationsData()
+  ]);
+}
+
+
+export default getData;

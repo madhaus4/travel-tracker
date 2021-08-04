964f07ac1c270ddfd12a3e58ed26a1520c5003d0
Maria DelSignore <maria.ad27@gmail.com>
2021-08-04 17:24:33 -0600

    Import test images

--- src/scripts.js
+++ src/scripts.js
@@ -7,9 +7,14 @@ import './css/main.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
 import './images/menu.png';
-import './images/boris-baldinger-eUFfY6cwjSU-unsplash.jpg';
+import './images/next.png'
 
-// import 'node_modules/@glidejs/glide/src/assets/sass/glide.core';
+
+import './images/boris-baldinger-eUFfY6cwjSU-unsplash.jpg';
+import './images/marek-piwnicki-3Exh4BdB2yA-unsplash.jpg';
+import './images/photo-1553882951-9c3dab4a50cb.jpeg';
+
+import Glide from '@glidejs/glide'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');

13979364b8a110be73cca7fd777d769fcc80c291
Maria DelSignore <maria.ad27@gmail.com>
2021-08-04 12:45:20 -0600

    Link correct css path

--- src/scripts.js
+++ src/scripts.js
@@ -6,5 +6,6 @@ import './css/main.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
+import './images/menu.png';
 
 console.log('This is the JavaScript entry file - your code begins here.');

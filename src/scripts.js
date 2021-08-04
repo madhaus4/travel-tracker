bdd89a6c8587566cdd0955daa76a3b4fb4e9ce36
Maria DelSignore <maria.ad27@gmail.com>
2021-08-04 17:09:02 -0600

    Install glide package

--- src/scripts.js
+++ src/scripts.js
@@ -7,5 +7,9 @@ import './css/main.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
 import './images/menu.png';
+import './images/boris-baldinger-eUFfY6cwjSU-unsplash.jpg';
+
+// import 'node_modules/@glidejs/glide/src/assets/sass/glide.core';
+
 
 console.log('This is the JavaScript entry file - your code begins here.');

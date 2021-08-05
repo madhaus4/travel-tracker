43cfbe4447b5f330643056657affab3c7d6f959b
Maria DelSignore <maria.ad27@gmail.com>
2021-08-04 23:03:08 -0600

    Update formatting

--- src/data/travelersData-sample.js
+++ src/data/travelersData-sample.js
@@ -1,18 +1,18 @@
 const sampleTravelers = [
   {
-    "id": 1,
-    "name": "Leila Thebeaud",
-    "travelerType": "photographer",
+    id: 1,
+    name: 'Leila Thebeaud',
+    travelerType: 'photographer',
   }, 
   {
-    "id": 2,
-    "name": "Rickie Jodlowski",
-    "travelerType": "relaxer",
+    id: 2,
+    name: 'Rickie Jodlowski',
+    travelerType: 'relaxer',
   }, 
   {
-    "id": 3,
-    "name": "Lannie Heynel",
-    "travelerType": "history buff"
+    id: 3,
+    name: 'Lannie Heynel',
+    travelerType: 'history buff'
   }
 ];
 

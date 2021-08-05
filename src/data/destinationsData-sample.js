059c1cbedc386468940c7eed26a0da1f4a3b9ac2
Maria DelSignore <maria.ad27@gmail.com>
2021-08-04 23:05:00 -0600

    Update formatting

--- src/data/destinationsData-sample.js
+++ src/data/destinationsData-sample.js
@@ -1,114 +1,114 @@
 const sampleDestinations = [
   {
-    "id": 6,
-    "destination": "Jakarta, Indonesia",
-    "estimatedLodgingCostPerDay": 70,
-    "estimatedFlightCostPerPerson": 890,
-    "image": "https://images.unsplash.com/photo-1555333145-4acf190da336?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-    "alt": "lit up city at night"
+    id: 6,
+    destination: 'Jakarta, Indonesia',
+    estimatedLodgingCostPerDay: 70,
+    estimatedFlightCostPerPerson: 890,
+    image: 'https://images.unsplash.com/photo-1555333145-4acf190da336?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    alt: 'lit up city at night'
   },
   {
-    "id": 9,
-    "destination": "Amsterdam, Netherlands",
-    "estimatedLodgingCostPerDay": 100,
-    "estimatedFlightCostPerPerson": 950,
-    "image": "https://images.unsplash.com/photo-1534351590666-13e3e96b5017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-    "alt": "canal with boats and trees and buildings along the side"
+    id: 9,
+    destination: 'Amsterdam, Netherlands',
+    estimatedLodgingCostPerDay: 100,
+    estimatedFlightCostPerPerson: 950,
+    image: 'https://images.unsplash.com/photo-1534351590666-13e3e96b5017?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    alt: 'canal with boats and trees and buildings along the side'
   },
   {
-    "id": 10,
-    "destination": "Toronto, Canada",
-    "estimatedLodgingCostPerDay": 90,
-    "estimatedFlightCostPerPerson": 450,
-    "image": "https://images.unsplash.com/photo-1535776142635-8fa180c46af7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2756&q=80",
+    id: 10,
+    destination: 'Toronto, Canada',
+    estimatedLodgingCostPerDay: 90,
+    estimatedFlightCostPerPerson: 450,
+    image: 'https://images.unsplash.com/photo-1535776142635-8fa180c46af7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2756&q=80',
   },
   {
-    "id": 14,
-    "destination": "Marrakesh, Morocco",
-    "estimatedLodgingCostPerDay": 70,
-    "estimatedFlightCostPerPerson": 830,
-    "image": "https://images.unsplash.com/photo-1517821362941-f7f753200fef?ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80",
-    "alt": "people buying oranges and other fruit from a street vendor"
+    id: 14,
+    destination: 'Marrakesh, Morocco',
+    estimatedLodgingCostPerDay: 70,
+    estimatedFlightCostPerPerson: 830,
+    image: 'https://images.unsplash.com/photo-1517821362941-f7f753200fef?ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80',
+    alt: 'people buying oranges and other fruit from a street vendor'
   },
   {
-    "id": 21,
-    "destination": "Tulum, Mexico",
-    "estimatedLodgingCostPerDay": 100,
-    "estimatedFlightCostPerPerson": 350,
-    "image": "https://images.unsplash.com/photo-1501619593928-bef49688c888?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-    "alt": "a donkey standing on the beach"
+    id: 21,
+    destination: 'Tulum, Mexico',
+    estimatedLodgingCostPerDay: 100,
+    estimatedFlightCostPerPerson: 350,
+    image: 'https://images.unsplash.com/photo-1501619593928-bef49688c888?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    alt: 'a donkey standing on the beach'
   },
   {
-    "id": 22,
-    "destination": "Rome, Italy",
-    "estimatedLodgingCostPerDay": 90,
-    "estimatedFlightCostPerPerson": 650,
-    "image": "https://images.unsplash.com/photo-1515542622106-78bda8ba0e5b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-    "alt": "people standing inside a colosseum during the day"
+    id: 22,
+    destination: 'Rome, Italy',
+    estimatedLodgingCostPerDay: 90,
+    estimatedFlightCostPerPerson: 650,
+    image: 'https://images.unsplash.com/photo-1515542622106-78bda8ba0e5b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    alt: 'people standing inside a colosseum during the day'
   },
   {
-    "id": 24,
-    "destination": "Vilnius, Lithuania",
-    "estimatedLodgingCostPerDay": 65,
-    "estimatedFlightCostPerPerson": 1100,
-    "image": "https://images.unsplash.com/photo-1549891472-991e6bc75d1e?ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80",
-    "alt": "overhead view of a city with a hot air balloon"
+    id: 24,
+    destination: 'Vilnius, Lithuania',
+    estimatedLodgingCostPerDay: 65,
+    estimatedFlightCostPerPerson: 1100,
+    image: 'https://images.unsplash.com/photo-1549891472-991e6bc75d1e?ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80',
+    alt: 'overhead view of a city with a hot air balloon'
   },
   {
-    "id": 26,
-    "destination": "London, England",
-    "estimatedLodgingCostPerDay": 100,
-    "estimatedFlightCostPerPerson": 1000,
-    "image": "https://images.unsplash.com/photo-1549471156-52ee71691122?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-    "alt": "city with bridge under night sky"
+    id: 26,
+    destination: 'London, England',
+    estimatedLodgingCostPerDay: 100,
+    estimatedFlightCostPerPerson: 1000,
+    image: 'https://images.unsplash.com/photo-1549471156-52ee71691122?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    alt: 'city with bridge under night sky'
   },
   {
-    "id": 27,
-    "destination": "San Francisco, California",
-    "estimatedLodgingCostPerDay": 175,
-    "estimatedFlightCostPerPerson": 200,
-    "image": "https://images.unsplash.com/photo-1501594907352-04cda38ebc29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80",
-    "alt": "golden gate bridge during the day time"
+    id: 27,
+    destination: 'San Francisco, California',
+    estimatedLodgingCostPerDay: 175,
+    estimatedFlightCostPerPerson: 200,
+    image: 'https://images.unsplash.com/photo-1501594907352-04cda38ebc29?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80',
+    alt: 'golden gate bridge during the day time'
   },
   {
-    "id": 35,
-    "destination": "Anchorage, Alaska",
-    "estimatedLodgingCostPerDay": 200,
-    "estimatedFlightCostPerPerson": 100,
-    "image": "https://images.unsplash.com/photo-1539545547102-90ae2c140089?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-    "alt": "man riding on kayak surrounded by mountains"
+    id: 35,
+    destination: 'Anchorage, Alaska',
+    estimatedLodgingCostPerDay: 200,
+    estimatedFlightCostPerPerson: 100,
+    image: 'https://images.unsplash.com/photo-1539545547102-90ae2c140089?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
+    alt: 'man riding on kayak surrounded by mountains'
   },
   {
-    "id": 39,
-    "destination": "Porto, Portugal",
-    "estimatedLodgingCostPerDay": 995,
-    "estimatedFlightCostPerPerson": 90,
-    "image": "https://images.unsplash.com/photo-1564644929137-34b018daf461?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1778&q=80",
-    "alt": "looking over the water on to a city on a hill"
+    id: 39,
+    destination: 'Porto, Portugal',
+    estimatedLodgingCostPerDay: 995,
+    estimatedFlightCostPerPerson: 90,
+    image: 'https://images.unsplash.com/photo-1564644929137-34b018daf461?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1778&q=80',
+    alt: 'looking over the water on to a city on a hill'
   },
   {
-    "id": 44,
-    "destination": "Caye Caulker, Belize",
-    "estimatedLodgingCostPerDay": 450,
-    "estimatedFlightCostPerPerson": 80,
-    "image": "https://images.unsplash.com/photo-1544525977-0a3bca9e560d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-    "alt": "boat on dock during daytime"
+    id: 44,
+    destination: 'Caye Caulker, Belize',
+    estimatedLodgingCostPerDay: 450,
+    estimatedFlightCostPerPerson: 80,
+    image: 'https://images.unsplash.com/photo-1544525977-0a3bca9e560d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+    alt: 'boat on dock during daytime'
   },
   {
-    "id": 47,
-    "destination": "Zürich, Switzerland",
-    "estimatedLodgingCostPerDay": 1100,
-    "estimatedFlightCostPerPerson": 110,
-    "image": "https://images.unsplash.com/photo-1515488764276-beab7607c1e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1390&q=80",
-    "alt": "landscape photo of cityscape"
+    id: 47,
+    destination: 'Zürich, Switzerland',
+    estimatedLodgingCostPerDay: 1100,
+    estimatedFlightCostPerPerson: 110,
+    image: 'https://images.unsplash.com/photo-1515488764276-beab7607c1e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1390&q=80',
+    alt: 'landscape photo of cityscape'
   },
   {
-    "id": 49,
-    "destination": "Castries, St Lucia",
-    "estimatedLodgingCostPerDay": 650,
-    "estimatedFlightCostPerPerson": 90,
-    "image": "https://images.unsplash.com/photo-1524478075552-c2763ea171b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80",
-    "alt": "aerial photography of rocky mountain under cloudy sky"
+    id: 49,
+    destination: 'Castries, St Lucia',
+    estimatedLodgingCostPerDay: 650,
+    estimatedFlightCostPerPerson: 90,
+    image: 'https://images.unsplash.com/photo-1524478075552-c2763ea171b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80',
+    alt: 'aerial photography of rocky mountain under cloudy sky'
   }
 ];
 

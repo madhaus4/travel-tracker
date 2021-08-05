742d042c0677642200441a2f83241120ee3406af
Maria DelSignore <maria.ad27@gmail.com>
2021-08-04 22:41:51 -0600

    Add sample trip data

---
+++ src/data/tripsData-sample.js
@@ -0,0 +1,124 @@
+const sampleTrips = [
+  {
+    "id": 1,
+    "userID": 4,
+    "destinationID": 49,
+    "travelers": 1,
+    "date": "2022/09/16",
+    "duration": 8,
+    "status": "approved",
+    "suggestedActivities": []
+  },
+  {
+    "id": 3,
+    "userID": 10,
+    "destinationID": 22,
+    "travelers": 4,
+    "date": "2022/05/22",
+    "duration": 17,
+    "status": "approved",
+    "suggestedActivities": []
+  },
+  {
+    "id": 21,
+    "userID": 12,
+    "destinationID": 10,
+    "travelers": 1,
+    "date": "2022/01/28",
+    "duration": 18,
+    "status": "approved",
+    "suggestedActivities": []
+  },
+  {
+    "id": 22,
+    "userID": 12,
+    "destinationID": 9,
+    "travelers": 4,
+    "date": "2022/05/01",
+    "duration": 19,
+    "status": "approved",
+    "suggestedActivities": []
+  },
+  {
+    "id": 23,
+    "userID": 4,
+    "destinationID": 24,
+    "travelers": 6,
+    "date": "2022/01/02",
+    "duration": 18,
+    "status": "approved",
+    "suggestedActivities": []
+  },
+  {
+    "id": 48,
+    "userID": 4,
+    "destinationID": 14,
+    "travelers": 6,
+    "date": "2021/02/10",
+    "duration": 8,
+    "status": "approved",
+    "suggestedActivities": []
+  },
+  {
+    "id": 81,
+    "userID": 10,
+    "destinationID": 26,
+    "travelers": 1,
+    "date": "2020/08/31",
+    "duration": 16,
+    "status": "approved",
+    "suggestedActivities": []
+  },
+  {
+    "id": 119,
+    "userID": 10,
+    "destinationID": 47,
+    "travelers": 5,
+    "date": "2020/05/28",
+    "duration": 20,
+    "status": "approved",
+    "suggestedActivities": []
+  },
+  {
+    "id": 142,
+    "userID": 12,
+    "destinationID": 39,
+    "travelers": 1,
+    "date": "2019/12/14",
+    "duration": 20,
+    "status": "approved",
+    "suggestedActivities": []
+  },
+  {
+    "id": 185,
+    "userID": 10,
+    "destinationID": 35,
+    "travelers": 4,
+    "date": "2019/09/02",
+    "duration": 16,
+    "status": "approved",
+    "suggestedActivities": []
+  },
+  {
+    "id": 186,
+    "userID": 4,
+    "destinationID": 44,
+    "travelers": 6,
+    "date": "2020/02/08",
+    "duration": 6,
+    "status": "approved",
+    "suggestedActivities": []
+  },
+  {
+    "id": 193,
+    "userID": 12,
+    "destinationID": 35,
+    "travelers": 1,
+    "date": "2020/11/09",
+    "duration": 19,
+    "status": "approved",
+    "suggestedActivities": []
+  },
+];
+
+export default sampleTrips;

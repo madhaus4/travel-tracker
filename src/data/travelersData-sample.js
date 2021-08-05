7b5ef13f7793d5fa88f1abc2e34ede0f18dbaaf2
Maria DelSignore <maria.ad27@gmail.com>
2021-08-04 22:42:08 -0600

    Add sample travelers data

---
+++ src/data/travelersData-sample.js
@@ -0,0 +1,19 @@
+const sampleTravelers = [
+  {
+    "id": 4,
+    "name": "Leila Thebeaud",
+    "travelerType": "photographer",
+  }, 
+  {
+    "id": 10,
+    "name": "Rickie Jodlowski",
+    "travelerType": "relaxer",
+  }, 
+  {
+    "id": 12,
+    "name": "Lannie Heynel",
+    "travelerType": "history buff"
+  }
+];
+
+export default sampleTravelers;
